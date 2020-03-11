791370de050962c4f7dbf8c06c52a30c638c5b91
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-03-11 21:55:30 +0500

    initial release

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const Post = require("./api/models/posts");
+const postsData = new Post();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+    }
+});
+
+const getExt = (mimetype) => {
+    switch(mimetype) {
+        case "image/png":
+            return '.png';
+        case "image/jpeg":
+            return '.jpg';
+    }
+};
+
+var upload = multer({ storage: storage });
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.use(express.json());
+
+app.use('/uploads', express.static('uploads'));
+
+app.get("/api/posts", (req, res) => {
+    res.status(200).send(postsData.get());
+});
+
+app.get("/api/posts/:postId", (req, res) => {
+    const postId = req.params.postId;
+    const posts = postsData.get();
+    const foundPost = posts.find((post) => post.id == postId);
+    if(foundPost){
+        res.status(200).send(foundPost);
+    } else {
+        res.status(404).send("Not Found");
+    }
+});
+
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path,
+        "added_date": `${Date.now()}`
+    };
+    postsData.add(newPost);
+    res.status(201).send(newPost);
+});
+
+
+app.listen(3311, () => console.log("Listening 3311"));
